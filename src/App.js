b013b68872f3ee0d0da4383ffbd0e878a6bc04bb
fade_luv <ssklvsk@gmail.com>
2022-12-17 20:42:16 +0300

    Настроен роутинг диалогов

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
           <Navbar />
           <div className="app-wrapper-content">
             <Routes>
-              <Route path="*" element={<Profile />} />
-              <Route path="/dialogs" element={<Dialogs />} />
+              <Route path="/profile*" element={<Profile />} />
+              <Route path="/dialogs/*" element={<Dialogs />} />
             </Routes>
           </div>
         </div>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,28 @@
+import { NavLink } from "react-router-dom";
 import classes from "./Dialogs.module.css";
 
 function Dialogs(params) {
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogs_items}>
-        <div className={classes.dialog + ' ' + classes.active}>Dmitry</div>
-        <div className={classes.dialog}>Sergey</div>
-        <div className={classes.dialog}>Svetlana</div>
-        <div className={classes.dialog}>Elena</div>
-        <div className={classes.dialog}>Victor</div>
-        <div className={classes.dialog}>Valera</div>
+        <div className={classes.dialog + " " + classes.active}>
+          <NavLink to="/dialogs/1">Dmitry</NavLink>
+        </div>
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/2">Sergey</NavLink>
+        </div>
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/3">Svetlana</NavLink>
+        </div>
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/4">Elena</NavLink>
+        </div>
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/5">Victor</NavLink>
+        </div>
+        <div className={classes.dialog}>
+          <NavLink to="/dialogs/6">Valera</NavLink>
+        </div>
       </div>
       <div className={classes.messages}>
         <div className={classes.message}>Hi!</div>
