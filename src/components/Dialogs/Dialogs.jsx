c3d3cd1c6a6d770e263af874005c7c146cbfef85
fade_luv <ssklvsk@gmail.com>
2022-12-17 20:13:12 +0300

    Верстка модуля диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,23 @@ import classes from "./Dialogs.module.css";
 
 function Dialogs(params) {
   return (
-    <div>Dialogs</div>
-  )
+    <div className={classes.dialogs}>
+      <div className={classes.dialogs_items}>
+        <div className={classes.dialog + ' ' + classes.active}>Dmitry</div>
+        <div className={classes.dialog}>Sergey</div>
+        <div className={classes.dialog}>Svetlana</div>
+        <div className={classes.dialog}>Elena</div>
+        <div className={classes.dialog}>Victor</div>
+        <div className={classes.dialog}>Valera</div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hi!</div>
+        <div className={classes.message}>Hi,how are you?</div>
+        <div className={classes.message}>I Love you!</div>
+        <div className={classes.message}>Thanks, bro!</div>
+      </div>
+    </div>
+  );
 }
 
 export default Dialogs;
